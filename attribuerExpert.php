c68793ece26bbdc4e4f44cb9971bc2399095534a
CamilleGAR <58259424+CamilleGAR@users.noreply.github.com>
2020-05-06 10:06:29 +0200

    Update attribuerExpert.php

--- attribuerExpert.php
+++ attribuerExpert.php
@@ -6,9 +6,17 @@
 				$selectExpert->execute([':role' => "Expert", 'domaine' => $demande['domaine']]);
 				while($exp = $selectExpert->fetch()){
 			?>
-		    		<option value=<?php $exp['nom'] ?>><?php echo $exp['nom'] ?></option>
+		    		<option value=<?php echo $exp['nom'] ?>><?php echo $exp['nom'] ?></option>
 				<?php } ?>
   		</optgroup>
   	</select><br/>	
-	<input type="submit" name="envoye" value="Envoyer"><br/>
+	<input type="submit" name=<?php echo "Valeur".$demande['id'] ?> value="Valeur"><br/>
 </form>
+
+<?php
+if(isset($_POST["Valeur".$demande['id']])){
+	if (!empty($_POST['domaine'])){
+		echo $_POST['domaine'];
+	}
+}
+?>
