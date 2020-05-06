f568494b613dfe95a7053614ddf4e7a6ca7e8fc2
CamilleGAR <58259424+CamilleGAR@users.noreply.github.com>
2020-05-06 05:06:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$nom = "Camille";
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -9,35 +9,68 @@
 </head>
 <body>
 
-
-	<a href="administrateur.php"> Page Admin </a>
-
-
-
-<!-- 
-	<p>Nom : <?= $nom; ?></p>
-
 	<?php
 		include 'database.php';
-		global $db;
+		global $bdd;
 	?>
 
-	<form method="post">
-		<input type="text" name="pseudo" id="pseudo" placeholder="pseudo" required>
-		<input type="submit" name="envoye" id="envoye">
+	<p>Indentification<p>
+	<form method="post"> 
+		<input type="text" name="pseudo" id="pseudo" placeholder="pseudo" required><br/>
+		<input type="text" name="password" id="password" placeholder="mot de passe" required><br/>
+		<input type="submit" name="envoye" id="envoye" value="S'identifier"><br/>
 	</form>
+
+	<a href="administrateur.php">Page Admin </a>
 
 	<?php
 		if(isset($_POST['envoye'])){
-			echo "Votre Pseudo : ".$_POST['pseudo']."<br/>";
-			$requete = $bdd->query("SELECT password FROM utilisateurs WHERE pseudo = ".$_POST['pseudo']);
+			extract($_POST);
 
-		while($utilisateur = $requete->fetch()){
-			echo "pseudo : ".$utilisateur['password'];
+			if (!empty($pseudo) && !empty($password)){
+
+				$selectCompte = $bdd->prepare("SELECT * FROM utilisateurs WHERE pseudo = :pseudo");
+				$selectCompte->execute([':pseudo' => $pseudo]);
+				$compte = $selectCompte->fetch();
+
+				if(($compte == true) && (password_verify($password, $compte['password']))){
+						$_SESSION['pseudo'] = $compte['pseudo'];
+						$_SESSION['nom'] = $compte['nom'];	
+						$_SESSION['prenom'] = $compte['prenom'];	
+						$_SESSION['role'] = $compte['role'];	
+						$_SESSION['email'] = $compte['email'];
+
+						if ($_SESSION['role'] == Administrateur){
+							header('Location: administrateur.php');
+  							exit();
+						}
+						if ($_SESSION['role'] == Expert){
+							header('Location: expert.php');
+  							exit();
+						}
+						if ($_SESSION['role'] == Professeur){
+							header('Location: professeur.php');
+  							exit();
+						}
+						if ($_SESSION['role'] == Eleve){
+							header('Location: eleve.php');
+  							exit();
+						}
+				}
+
+				else{
+					echo 'Mauvais identifiants';
+				}
+
+			}
+
+			else{
+				echo "champs vides";
+			}
+
 		}
-		}
+
 	?>
- -->
 
 </body>
 </html>
