ffefd28501cc31fe5c489a0ddcc9b0578dc39f10
CamilleGAR <58259424+CamilleGAR@users.noreply.github.com>
2020-05-09 05:14:02 +0200

    Update professeur.php

--- professeur.php
+++ professeur.php
@@ -1,1 +1,30 @@
 
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<?php
+		include 'database.php';
+		global $bdd;
+		echo "Bonjour ".$_SESSION['prenom'];
+		include 'deconnection.php';
+	?>
+
+<?php
+	$selectDemande = $bdd->prepare("SELECT * FROM utilisateurs, aide WHERE utilisateurs.pseudo = aide.eleve AND expert IS NULL AND professeur IS NULL AND aide.domaine = :domaineProf");
+	$selectDemande->execute([':domaineProf' => $_SESSION['domaine']]);
+	while ($demande = $selectDemande->fetch()){
+		echo "demande de ".$demande['prenom']." ".$demande['nom']." en ".$demande['domaine']."<br/>";
+		include 'attribuerExpert.php';
+	}
+?>
+
+</body>
+</html>
