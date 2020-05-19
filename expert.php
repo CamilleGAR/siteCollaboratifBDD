456dfdd229870eab438fbcf07cbf96060833f607
CamilleGAR <58259424+CamilleGAR@users.noreply.github.com>
2020-05-19 18:05:21 +0200

    Update expert.php

--- expert.php
+++ expert.php
@@ -1,8 +1,29 @@
-
 <?php
 session_start();
-include 'database.php';
-global $bdd;
-include 'demandesEnvoyees.php';
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<a href="index.php">Retour </a><br/>
+
+	<?php
+	if (isset($_SESSION['role']) and $_SESSION['role'] == 'Expert'){
+		include 'database.php';
+		global $bdd;
+		echo "Bonjour ".$_SESSION['prenom'];
+		include 'deconnection.php';
+		include 'demandesEnvoyees.php';
+	}
+
+	else{
+		echo 'Vous n etes pas connectes en tant qu expert';
+	}
+	?>
+
+</body>
+</html>
