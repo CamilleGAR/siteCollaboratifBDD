30bba8865812e861373e1db24ee0aa1868c274b3
CamilleGAR <58259424+CamilleGAR@users.noreply.github.com>
2020-05-19 18:03:20 +0200

    Update professeur.php

--- professeur.php
+++ professeur.php
@@ -1,4 +1,3 @@
-
 <?php
 session_start();
 ?>
@@ -10,21 +9,27 @@ session_start();
 </head>
 <body>
 
-	<?php
-		include 'database.php';
-		global $bdd;
-		echo "Bonjour ".$_SESSION['prenom'];
-		include 'deconnection.php';
-	?>
 
-<?php
-	$selectDemande = $bdd->prepare("SELECT * FROM utilisateurs, aide WHERE utilisateurs.pseudo = aide.eleve AND expert IS NULL AND professeur IS NULL AND aide.domaine = :domaineProf");
-	$selectDemande->execute([':domaineProf' => $_SESSION['domaine']]);
-	while ($demande = $selectDemande->fetch()){
-		echo "demande de ".$demande['prenom']." ".$demande['nom']." en ".$demande['domaine']."<br/>";
-		include 'attribuerExpert.php';
-	}
-?>
+	<a href="index.php">Retour </a><br/>
+
+	<?php
+		if (isset($_SESSION['role']) and $_SESSION['role'] == 'Professeur'){
+				include 'database.php';
+				global $bdd;
+				echo "Bonjour ".$_SESSION['prenom'];
+				include 'deconnection.php';
+
+			$selectDemande = $bdd->prepare("SELECT * FROM utilisateurs, aide WHERE utilisateurs.pseudo = aide.eleve AND expert IS NULL AND professeur IS NULL AND aide.domaine = :domaineProf");
+			$selectDemande->execute([':domaineProf' => $_SESSION['domaine']]);
+			while ($demande = $selectDemande->fetch()){
+				echo "demande de ".$demande['prenom']." ".$demande['nom']." en ".$demande['domaine']."<br/>";
+				include 'attribuerExpert.php';
+			}
+		}
+		else{
+			echo 'Vous n etes pas connectes en tant que professeur';
+		}
+	?>
 
 </body>
 </html>
