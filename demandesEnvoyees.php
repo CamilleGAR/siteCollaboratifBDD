b28fdb88ff7217300a607eeffc39daa3825c7522
CamilleGAR <58259424+CamilleGAR@users.noreply.github.com>
2020-05-19 18:03:43 +0200

    Update demandesEnvoyees.php

--- demandesEnvoyees.php
+++ demandesEnvoyees.php
@@ -2,7 +2,7 @@
 
 if ($_SESSION['role'] == 'Expert'){
 
-	echo "Demandes en attentes <br/><br/>";
+	echo "<br/>Demandes en attentes <br/>";
 	$demandesEnvoyees = $bdd->prepare("SELECT aide.id, utilisateurs.nom, utilisateurs.prenom FROM aide, utilisateurs WHERE aide.expert = :expert AND aide.eleve = utilisateurs.pseudo AND etat = 'En attente'");
 	$demandesEnvoyees ->execute([':expert' => $_SESSION['pseudo']]);
 	while ($demande = $demandesEnvoyees->fetch()){
@@ -10,7 +10,7 @@ if ($_SESSION['role'] == 'Expert'){
 		<a href = <?php echo 'dossier.php?id='.$demande['id'] ?> > <?php echo 'demande de '.$demande['prenom'].' '.$demande['nom'].'<br/>'?> </a>
 		<?php
 	}
-	echo "Reponses envoyées <br/><br/>";
+	echo "<br/>Reponses envoyées <br/>";
 	$demandesEnvoyees = $bdd->prepare("SELECT aide.domaine, aide.id FROM aide, utilisateurs WHERE aide.expert = :expert AND aide.eleve = utilisateurs.pseudo AND etat = 'Reponse recue'");
 	$demandesEnvoyees ->execute([':expert' => $_SESSION['pseudo']]);
 	while ($demande = $demandesEnvoyees->fetch()){
@@ -27,7 +27,7 @@ if ($_SESSION['role'] == 'Expert'){
 
 if ($_SESSION['role'] == 'Eleve'){
 
-	echo "Reponses reçues <br/><br/>";
+	echo "<br/>Reponses reçues <br/>";
 	$demandesEnvoyees = $bdd->prepare("SELECT aide.domaine, aide.id FROM aide, utilisateurs WHERE aide.eleve = :eleve AND aide.eleve = utilisateurs.pseudo AND etat = 'Reponse recue'");
 	$demandesEnvoyees ->execute([':eleve' => $_SESSION['pseudo']]);
 	while ($demande = $demandesEnvoyees->fetch()){
@@ -36,7 +36,7 @@ if ($_SESSION['role'] == 'Eleve'){
 		<?php
 	}
 
-	echo "Demandes assignées, en attente de réponse. <br/><br/>";
+	echo "<br/>Demandes redirigées vers un expert, en attente de réponse. <br/>";
 	$demandesEnvoyees = $bdd->prepare("SELECT aide.domaine, aide.id FROM aide, utilisateurs WHERE aide.eleve = :eleve AND aide.eleve = utilisateurs.pseudo AND etat = 'En attente'");
 	$demandesEnvoyees ->execute([':eleve' => $_SESSION['pseudo']]);
 	while ($demande = $demandesEnvoyees->fetch()){
@@ -45,7 +45,7 @@ if ($_SESSION['role'] == 'Eleve'){
 		<?php
 	}
 
-	echo "Demandes envoyées. En attente d'assignation d'un expert. <br/><br/>";
+	echo "<br/>Demandes envoyées. En attente d'attribution d'un expert. <br/>";
 	$demandesEnvoyees = $bdd->prepare("SELECT aide.domaine, aide.id FROM aide, utilisateurs WHERE aide.eleve = :eleve AND aide.eleve = utilisateurs.pseudo AND etat = 'Demande envoyee'");
 	$demandesEnvoyees ->execute([':eleve' => $_SESSION['pseudo']]);
 	while ($demande = $demandesEnvoyees->fetch()){
@@ -57,4 +57,5 @@ if ($_SESSION['role'] == 'Eleve'){
 }
 
 ?>
+
 
