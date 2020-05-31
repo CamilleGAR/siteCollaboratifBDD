24992fc695247144240e820df86eb055cebaf207
CamilleGAR <58259424+CamilleGAR@users.noreply.github.com>
2020-05-31 18:57:15 +0200

    Update dossier.php

--- dossier.php
+++ dossier.php
@@ -87,13 +87,20 @@ if ($_SESSION['role'] == 'Eleve'){
     	<input type="submit" value="Retour" />
 	</form>
 	<?php
-	$demande = $bdd->prepare("SELECT * FROM aide, utilisateurs WHERE aide.expert = utilisateurs.pseudo AND aide.id = :id AND eleve = :eleve");
+
+	$demande = $bdd->prepare("SELECT * FROM aide WHERE id = :id AND eleve = :eleve");
 	$demande ->execute([':id' => $_GET['id'], ':eleve' => $_SESSION['pseudo']]);
 	$nb = $demande->rowCount();
 
+	$demandeLienExpert = $bdd->prepare("SELECT * FROM aide, utilisateurs WHERE aide.expert = utilisateurs.pseudo AND aide.id = :id AND eleve = :eleve");
+	$demandeLienExpert ->execute([':id' => $_GET['id'], ':eleve' => $_SESSION['pseudo']]);
+	$nbLienExpert = $demandeLienExpert->rowCount();
+
 	if ($nb > 0){
-		$getEmail = $demande->fetch();
-		echo "adresse mail de l'expert : ".$getEmail['email'];
+		if ($nbLienExpert > 0){
+			$getEmail = $demandeLienExpert->fetch();
+			echo "adresse mail de l'expert : ".$getEmail['email'];
+		}
 		?>
 	<form method="post"> 
 		<textarea name="message" rows="8" cols="45" placeholder="Votre réponse"></textarea><br/>
@@ -105,13 +112,13 @@ if ($_SESSION['role'] == 'Eleve'){
 		if (!empty($message)){
 			$requete = $bdd->prepare("INSERT INTO reponse(id_demande, role, texte) VALUES(:id_demande, :role, :texte)");
 			$requete->execute([':id_demande' => $_GET['id'], ':role' => 'Eleve', ':texte' => $message]);
+			$modif = $bdd->prepare("UPDATE aide SET etat = 'En attente' WHERE id = :id");
+			$modif->execute([':id' => $_GET['id']]);
 		}
 	}
 
 		$reponses = $bdd->prepare("SELECT * FROM reponse WHERE id_demande = :id ORDER BY id DESC");
 		$reponses ->execute([':id' => $_GET['id']]);
-		$modif = $bdd->prepare("UPDATE aide SET etat = 'En attente' WHERE id = :id");
-		$modif->execute([':id' => $_GET['id']]);
 
 		while ($reponse = $reponses->fetch()){
 
